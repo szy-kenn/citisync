44a2f887ee9a6189534cc0bcee380e8eeaedad68
Ken <120155609+szy-kenn@users.noreply.github.com>
2025-01-12 09:40:03 +0800

    add

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export const dynamic = 'force-dynamic';
 export default function Page() {
 
   return (
-    <div className="min-h-screen bg-[url('/background.svg')] bg-no-repeat bg-cover bg-blue-dark text-white">
+    <div className="min-h-screen bg-[url('https://vsbkxysdsqapomfudqih.supabase.co/storage/v1/object/public/chataptptpt/Background.svg')] bg-no-repeat bg-cover bg-blue-dark text-white">
       <div className='flex flex-col items-center pt-20'>
         <h1 className='text-5xl font-bold'>CitiSync</h1>
         <p className='text-sm'>Your City, Your Voice, Our Sync</p>
